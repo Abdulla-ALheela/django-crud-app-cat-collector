78c3e7450719c7ba547b8959b17497e4c52a5b95
Abdulla Alheela <Abdulla.alheela@gmail.com>
2025-04-20 20:40:09 +0300

    complete feeding class

--- main_app/views.py
+++ main_app/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from .models import Cat
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .forms import FeedingForm
+from django.shortcuts import render, redirect
 # Import HttpResponse to send text-based responses
 
 
@@ -25,7 +27,8 @@ def cat_index(request):
 
 def cat_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
-    return render(request, 'cats/detail.html', {'cat': cat})
+    feeding_form = FeedingForm()
+    return render(request, 'cats/detail.html', {'cat': cat, 'feeding_form': feeding_form })
 
 class CatCreate(CreateView):
     model = Cat
@@ -38,3 +41,12 @@ class CatUpdate(UpdateView):
 class CatDelete(DeleteView):
     model = Cat
     success_url = '/cats/'
+
+
+def add_feeding(request, cat_id):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.cat_id = cat_id
+        new_feeding.save()
+    return redirect('cat-detail', cat_id=cat_id)
