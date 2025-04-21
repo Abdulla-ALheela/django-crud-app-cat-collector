dc2ca4b6cacf9c8789dac9888921bbc433b05025
Abdulla Alheela <Abdulla.alheela@gmail.com>
2025-04-21 20:17:28 +0300

    complete app

--- main_app/views.py
+++ main_app/views.py
@@ -2,10 +2,15 @@
 
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
+from django.contrib.auth.views import LoginView
 from .models import Cat
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .forms import FeedingForm
 from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Import HttpResponse to send text-based responses
 
 
@@ -19,30 +24,40 @@ def about(request):
     # Send a simple HTML response
   return render(request, 'about.html')
 
-
+@login_required
 def cat_index(request):
-    cats = Cat.objects.all()  # look familiar?
+    cats = Cat.objects.filter(user=request.user)
     return render(request, 'cats/index.html', {'cats': cats})
 
-
+@login_required
 def cat_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
     feeding_form = FeedingForm()
     return render(request, 'cats/detail.html', {'cat': cat, 'feeding_form': feeding_form })
 
-class CatCreate(CreateView):
+
+class CatCreate(LoginRequiredMixin,CreateView):
     model = Cat
-    fields = '__all__'
+    fields = ['name', 'breed', 'description', 'age']
+
+    # This inherited method is called when a
+    # valid cat form is being submitted
+    def form_valid(self, form):
+        # Assign the logged in user (self.request.user)
+        form.instance.user = self.request.user  # form.instance is the cat
+        # Let the CreateView do its job as usual
+        return super().form_valid(form)
+
  
-class CatUpdate(UpdateView):
+class CatUpdate(LoginRequiredMixin,UpdateView):
     model = Cat
     fields = ['breed', 'description', 'age']
 
-class CatDelete(DeleteView):
+class CatDelete(LoginRequiredMixin,DeleteView):
     model = Cat
     success_url = '/cats/'
 
-
+@login_required
 def add_feeding(request, cat_id):
     form = FeedingForm(request.POST)
     if form.is_valid():
@@ -50,3 +65,32 @@ def add_feeding(request, cat_id):
         new_feeding.cat_id = cat_id
         new_feeding.save()
     return redirect('cat-detail', cat_id=cat_id)
+
+class Home(LoginView):
+    template_name = 'home.html'
+
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        # This is how to create a 'user' form object
+        # that includes the data from the browser
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # This will add the user to the database
+            user = form.save()
+            # This is how we log a user in
+            login(request, user)
+            return redirect('cat-index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    # A bad POST or a GET request, so render signup.html with an empty form
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'signup.html', context)
+    # Same as: 
+    # return render(
+    #     request, 
+    #     'signup.html',
+    #     {'form': form, 'error_message': error_message}
+    # )
